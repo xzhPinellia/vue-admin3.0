b635472d2771095ee627ac60d86adb68ad708787
Pinallia <857667863@qq.com>
2020-01-10 13:35:01 +0800

    登陆 注册 后台首页布局

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueCompositionApi from "@vue/composition-api";
+import App from "./App.vue";
+import store from "./store/index.js";
+import router from "./router/index.js";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+// 自定义全局方法
+import "./icons";
+
+Vue.use(ElementUI);
+Vue.use(VueCompositionApi);
+Vue.config.productionTip = false;
+
+// runtime（运行模式）
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
