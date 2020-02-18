69138f6e006bbd8ec513e0acc017602cf78afe4e
Pinallia <857667863@qq.com>
2020-02-18 13:51:18 +0800

    域名修改

--- vue.config.js
+++ vue.config.js
@@ -65,7 +65,7 @@ module.exports = {
     hotOnly: false,
     proxy: {
       '/devApi': {
-          target: "http://www.web-jshtml.cn/productapi/token", //API服务器的地址  http://www.web-jshtml.cn/api
+          target: "http://www.web-jshtml.cn/dependenciesapi/token", //API服务器的地址  http://www.web-jshtml.cn/api
           changeOrigin: true,
           pathRewrite: {
               '^/devApi': ''
